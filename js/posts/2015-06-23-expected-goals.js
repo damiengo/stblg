0cad836953dd9acba6e50fcb80d167fe1da9d76b
damiengo <dam.gou@gmail.com>
2015-07-16 14:16:24 +0000

    Field

--- js/posts/2015-06-23-expected-goals.js
+++ js/posts/2015-06-23-expected-goals.js
@@ -61,10 +61,10 @@ function expgField(element) {
                      {"x": fieldStartX+fieldWidth, "y": fieldStartY+fieldHeight},
                      {"x": fieldStartX, "y": fieldStartY+fieldHeight},
                      {"x": fieldStartX, "y": fieldStartY},
-                     {"x": fieldStartX+202.6, "y": fieldStartY},
-                     {"x": fieldStartX+202.6, "y": fieldStartY+204},
-                     {"x": fieldStartX+757.4, "y": fieldStartY+204},
-                     {"x": fieldStartX+757.4, "y": fieldStartY}];
+                     {"x": fieldStartX+193, "y": fieldStartY},
+                     {"x": fieldStartX+193, "y": fieldStartY+183},
+                     {"x": fieldStartX+727, "y": fieldStartY+183},
+                     {"x": fieldStartX+727, "y": fieldStartY}];
     var lineFunction = d3.svg.line()
                              .x(function(d) { return d.x; })
                              .y(function(d) { return d.y; })
@@ -76,18 +76,30 @@ function expgField(element) {
                         .attr("fill", "none");
 
     // The center circle
-    var circleSelection = svg.append("circle")
+    /*var circleSelection = svg.append("circle")
                          .attr("cx", fieldStartX+(fieldWidth/2))
                          .attr("cy", fieldStartY+fieldHeight)
                          .attr("r", 92.5)
                          .style("stroke-opacity", 1)
                          .style("stroke-width", 2)
                          .style("fill-opacity", "0")
-                         .style("stroke", "#FFF");
+                         .style("stroke", "#FFF");*/
 
-    // The circle arc
+    // The penalty area circle arc
     svg.append("path")
-                  .attr("d", "M "+(fieldStartX+408)+","+(fieldStartY+204)+" C "+(fieldStartX+303)+","+(fieldStartY+66)+" "+(fieldStartX+303)+","+(fieldStartY+83)+" "+(fieldStartX+600)+","+(fieldStartY+204))
+                  .attr("d", "M "+(fieldStartX+(fieldWidth/2)-92.5)+","+(fieldStartY+fieldHeight)+
+                            " A 1,1 0 0,1"+
+                              " "+(fieldStartX+(fieldWidth/2)+92.5)+","+(fieldStartY+fieldHeight))
+                  .style("stroke-opacity", 1)
+                  .style("stroke-width", 2)
+                  .style("fill-opacity", "0")
+                  .style("stroke", "#FFF");
+
+    // The penalty area circle arc
+    svg.append("path")
+                  .attr("d", "M "+(fieldStartX+364)+","+(fieldStartY+183)+
+                            " A 120,120 0 0,0"+
+                              " "+(fieldStartX+556)+","+(fieldStartY+183))
                   .style("stroke-opacity", 1)
                   .style("stroke-width", 2)
                   .style("fill-opacity", "0")
@@ -99,7 +111,7 @@ function expgField(element) {
       var grData = d3.nest()
           .key(function(d) { return Math.floor(d.x); })
           .key(function(d) { return Math.floor(d.y); })
-          .rollup(function(l){
+         .rollup(function(l){
               return {
                   "length" : l.length,
                   "mean"   : d3.mean(l, function(d) {return d.predict;})
@@ -136,7 +148,7 @@ function expgField(element) {
                       .domain([d3.min(data, function(d) { return d.mean }), d3.max(data, function(d) { return d.mean })])
                       .range([0,1]);
 
-      var maxLen = 10;
+      var maxLen = 30;
       var lengthes = d3.scale.linear()
                       .domain([d3.min(data, function(d) { return d.length }), d3.max(data, function(d) { return d.length })])
                       .range([0, 30]);
@@ -152,7 +164,7 @@ function expgField(element) {
           .attr("ry", 5)
           .attr("width", function(d) { return (lengthes(d.length) > maxLen)? maxLen : lengthes(d.length); })
           .attr("height", function(d) { return (lengthes(d.length) > maxLen)? maxLen : lengthes(d.length); })
-          .attr("x", function(d) { return fieldStartX+x(d.y); })
+          .attr("x", function(d) { return x(d.y); })
           .attr("y", function(d) { return fieldStartY+y(d.x); })
           .style("fill", function(d) { return heatmapColour(c(d.mean)); });
 
@@ -168,29 +180,30 @@ Field path:
 3: V 16
 4: H 16
 5: V 350
-6: M16,86.47399999999999 (ligne but)
-7: L92.16000000000001,86.47399999999999 (angle 1)
-8: L92.16000000000001,279.526 (angle 2)
-9: L16,279.526 (ligne but)
-10: M16,138.912
-11: L41.984,138.912
-12: L41.984,227.08800000000002
-13: L16,227.08800000000002
+6: M 16,86.47399999999999 (ligne but)
+7: L 92.16000000000001,86.47399999999999 (angle 1)
+8: L 92.16000000000001,279.526 (angle 2)
+9: L 16,279.526 (ligne but)
+10: M 16,138.912
+11: L 41.984,138.912
+12: L 41.984,227.08800000000002
+13: L 16,227.08800000000002
 14: M 92.16000000000001,147.7296
 15: C 110.08000000000001,164.4296
       110.08000000000001,201.5704
       92.16000000000001,218.27040000000002
 M 240.00000000000003,16
-V 350M464.00000000000006,86.47399999999999
-L387.84000000000003,86.47399999999999
-L387.84000000000003,279.526
-L464.00000000000006,279.526
-M464.00000000000006,138.912
-L438.0160000000001,138.912
-L438.0160000000001,227.08800000000002
-L464.00000000000006,227.08800000000002
-M387.84000000000003,147.7296
-C369.92,164.4296
-369.92,201.5704
-387.84000000000003,218.27040000000002
+V 350
+M 464.00000000000006,86.47399999999999
+L 387.84000000000003,86.47399999999999
+L 387.84000000000003,279.526
+L 464.00000000000006,279.526
+M 464.00000000000006,138.912
+L 438.0160000000001,138.912
+L 438.0160000000001,227.08800000000002
+L 464.00000000000006,227.08800000000002
+M 387.84000000000003,147.7296
+C 369.92,164.4296
+  369.92,201.5704
+  387.84000000000003,218.27040000000002
 */
